14ab19aae44ec1701015f96d9aefc7a610b1e8a9
Ashish Shukla <ashish.shukla2@globallogic.com>
2022-03-23 17:27:58 +0530

    added edit todo functionality

--- src/modules/todo/todoList.js
+++ src/modules/todo/todoList.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { IconButton, Checkbox } from "@mui/material";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
@@ -6,8 +6,27 @@ import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import { DeleteOutline } from "@mui/icons-material";
+import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import { TextField, Button } from "@mui/material";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 export default function TodoList({ todoDataList, updateTodo, deleteTodo }) {
+  const [editFormOpened, seteditFormOpened] = useState(false);
+  const [edittedTodo, setEdittedTodo] = useState(null);
+
   const handleToggle = (e) => {
     let todoID = e.target.offsetParent.id;
     if (!todoID) {
@@ -23,6 +42,84 @@ export default function TodoList({ todoDataList, updateTodo, deleteTodo }) {
       };
       updateTodo(editPayload, todoID);
     }
+  };
+
+  const handleClose = () => {
+    seteditFormOpened(false);
+  };
+
+  const handleSubmitEdit = () => {
+    const editPayload = {
+      todo: edittedTodo.todo,
+      done: edittedTodo.done,
+    };
+    console.log("edittedTodo on submit is", edittedTodo);
+    updateTodo(editPayload, edittedTodo?._id);
+  };
+
+  const handleOnChange = (e) => {
+    setEdittedTodo(e.target.value);
+    setEdittedTodo({
+      ...edittedTodo,
+      todo: e.target.value,
+    });
+  };
+
+  const EditContainer = () => {
+    return (
+      <Modal
+        open={editFormOpened}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <form>
+            <TextField
+              id="todo-text"
+              sx={{
+                width: "75%",
+              }}
+              label="Edit Todo"
+              variant="outlined"
+              autoFocus={true}
+              size="medium"
+              onChange={handleOnChange}
+              value={edittedTodo?.todo}
+            />
+            <Button
+              variant="contained"
+              sx={{ margin: "5px 6px 0px 6px", width: "18%" }}
+              type="submit"
+              onClick={handleSubmitEdit}
+              size="large"
+            >
+              Save
+            </Button>
+          </form>
+        </Box>
+      </Modal>
+    );
+  };
+
+  const editTodo = (e) => {
+    let todoID = e.target.offsetParent?.id;
+    console.log("todoID is", todoID);
+    if (!todoID) {
+      todoID = e.target.id;
+    }
+
+    if (todoID) {
+      const clickedTodo = todoDataList.find((item) => {
+        return item._id === todoID;
+      });
+      console.log("clickedTodo is", clickedTodo);
+      if (clickedTodo) {
+        setEdittedTodo(clickedTodo);
+      }
+    }
+
+    seteditFormOpened(true);
   };
 
   return (
@@ -41,14 +138,24 @@ export default function TodoList({ todoDataList, updateTodo, deleteTodo }) {
             id={value._id}
             key={value._id}
             secondaryAction={
-              <IconButton
-                id={value._id}
-                edge="end"
-                aria-label="comments"
-                onClick={(e) => deleteTodo(e)}
-              >
-                <DeleteOutline id={value._id}/>
-              </IconButton>
+              <>
+                <IconButton
+                  id={value?._id}
+                  edge="end"
+                  aria-label="comments"
+                  onClick={(e) => deleteTodo(e)}
+                >
+                  <DeleteOutline id={value?._id} />
+                </IconButton>
+                <IconButton
+                  id={value?._id}
+                  edge="end"
+                  aria-label="comments"
+                  onClick={(e) => editTodo(e)}
+                >
+                  <EditOutlinedIcon id={value?._id} />
+                </IconButton>
+              </>
             }
             sx={{ padding: "0px" }}
             divider={true}
@@ -74,6 +181,7 @@ export default function TodoList({ todoDataList, updateTodo, deleteTodo }) {
           </ListItem>
         );
       })}
+      <EditContainer />
     </List>
   );
 }
